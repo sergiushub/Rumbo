cf290427e9a208a4eb6d4fa15f4c8395a5f09583
Sergio Domínguez <sergio.dmgz@gmail.com>
2017-02-03 22:36:05 +0100

    First Commit

---
+++ src/test/java/com/app/SearchEngineTest.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SearchEngineTest {
+
+	@Test
+	public void test() {
+		String helloWorld = new String("Hello, World!");
+		assertEquals(helloWorld,"Hello, World!");
+	}
+
+}
