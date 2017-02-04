974ecf16cb41b99f2a4d20d9e0b9f88f4b6decce
Sergio Domínguez <sergio.dmgz@gmail.com>
2017-02-04 14:02:14 +0100

    Input Data 2

--- src/test/java/com/app/SearchEngineTest.java
+++ src/test/java/com/app/SearchEngineTest.java
@@ -1,6 +1,11 @@
 package com.app;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Test;
 
@@ -8,9 +13,31 @@ public class SearchEngineTest {
 
 	@Test
 	public void test() {
+
+		inputData();
+
 		String helloWorld = new String("Hello, World!");
 		System.out.println("Hello!");
-		assertEquals(helloWorld,"Hello, World!");
+		assertEquals(helloWorld, "Hello, World!");
+	}
+
+	private void inputData() {
+		String cadena;
+		FileReader f;
+		try {
+			f = new FileReader("../test/resources/com/data/FlightConnectionPrice.dat");
+			BufferedReader b = new BufferedReader(f);
+			while ((cadena = b.readLine()) != null) {
+				System.out.println(cadena);
+			}
+			b.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
