e92cfc51ad387035498b66a936218d58cc7315ec
Sergio Domínguez <sergio.dmgz@gmail.com>
2017-02-04 13:56:45 +0100

    Data Input

--- src/main/java/com/app/SearchEngine.java
+++ src/main/java/com/app/SearchEngine.java
@@ -5,7 +5,13 @@ import java.util.Date;
 public class SearchEngine {
 
 	public double searchFlight(String airportOrigin, String airportDestination, Date departureDate,
-			Integer adultPassenger, Integer childPassenger, Integer infantPassenger) {
+			Integer adultPassenger, Integer childPassenger, Integer infantPassenger) throws Exception {
+		
+		//Comprobamos que el aeropuerto de Origen existe
+		if (!airportOrigin.isEmpty()) {
+			throw new Exception();
+		}
+		
 		return 1;
 	}
 	
